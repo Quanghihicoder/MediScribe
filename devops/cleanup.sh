d2d566766bfc812dcc2c9a6989bd459abfc5fed6
Quanghihicoder <hoangquang2508@gmail.com>
2025-07-23 15:48:14 +1000

    cleanup script

---
+++ devops/cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Safety destroy
+# ./destroy.sh
+
+# Delete terraform state bucket
+aws s3 rb s3://mediscribe-terraform --force --region ap-southeast-2
+
+# Delete terraform lock table
+aws dynamodb delete-table \
+  --table-name mediscribe-terraform-lock \
+  --region ap-southeast-2 \
+  --no-cli-pager
